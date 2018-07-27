7f8b85b944f9328ad6e737671d72fb6e15a559aa
12helenl <12helenl@gmail.com>
2018-07-27 22:00:14 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,11 +3,17 @@ class Cat
   attr_reader :name
   attr_writer :nervous_mood
   
-  def initialize (name, nervous_mood)
+  def initialize (name, mood)
     @name = name
-    @nervous_mood = nervous_mood
+    @nervous_mood = mood
   end
-
+  
+  def mood(new_mood)
+    @mood = new_mood
+  end
+  
 end
 
 cat_1 = Cat.new ("Crookshank")
+puts cat_1.name 
+puts cat_1.mood = "Happy"
